4abd4d223a81770ec6091de2dd404c6ac296308b
ari23yan <ariyan13723@yahoo.com>
2024-07-26 01:59:20 +0330

    Add project files.

---
+++ Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PelicanManagementUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ViewComponents/SideBarViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PelicanManagementUi.ViewComponents
+{
+    public class SideBarViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //var categories = await _categoryRepository.GetAllParentCategoriesAsync();
+            //var /*viewModel*/ = _mapper.Map<List<GetAllCategoryViewModel>>(categories);
+            //return View("Category", viewModel);
+
+
+            return View("SideBar");
+        }
+    }
+}
